640ce8c370aec379057d4715495271f0a84afb12
noasharvit <58943710+noasharvit@users.noreply.github.com>
2023-03-29 18:43:41 +0300

    final cosmetics

--- src/Info.js
+++ src/Info.js
@@ -5,7 +5,9 @@ export default function Info({ src, text }) {
   return (
     <div style={{ width: "200px", height: "200px", display: 'flex', flexDirection: 'column', justifyContent: 'start', alignItems: 'center' }}>
       <div className="info">
+      <div style={{ backgroundColor: '#656565', padding: '1px' }}>
         <img src={src} alt="Logo" style={{ maxWidth: '100%', maxHeight: '100%', display: 'block' }} />
+        </div>
       </div>
       <div className="info-text">
         {text}
